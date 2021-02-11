06623b8e37b3fbda540f907ed7a4915af4e324b4
DanielH76 <daniel.hammershoi@gmail.com>
2021-02-11 15:41:17 +0100

    Opdateret til database

--- TheMovies.Udleveret/ModelsDB/Models/CinemaMovieShowBooking.cs
+++ TheMovies.Udleveret/ModelsDB/Models/CinemaMovieShowBooking.cs
@@ -6,6 +6,25 @@ namespace TheMovies.Models
 {
     public class CinemaMovieShowBooking
     {
-        // Implement this!
+        public int BookingID { get; set; }
+        public string CinemaName { get; set; }
+        public string CinemaTown { get; set; }
+        public DateTime ShowDateTime { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieGenre { get; set; }
+        public int MovieDuration { get; set; }  // The duration of the movie in minutes
+        public string MovieDirector { get; set; }
+        public DateTime MovieReleaseDate { get; set; }
+        public string BookingMail { get; set; }
+        public string BookingPhone { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cinema: {CinemaName}, Town: {CinemaTown}, Show: {ShowDateTime}, Movie: {MovieTitle}, Genre: {MovieGenre}, Duration: {MovieDuration}, Director: {MovieDirector}, Release: {MovieReleaseDate}, Booking mail: {BookingMail}, Booking phone: {BookingPhone}";
+        }
+        public CinemaMovieShowBooking()
+        {
+
+        }
     }
 }
--- TheMovies.Udleveret/ModelsDB/Repos/CinemaMovieShowBookingRepo.cs
+++ TheMovies.Udleveret/ModelsDB/Repos/CinemaMovieShowBookingRepo.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using TheMovies.Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TheMovies.Repos
 {
@@ -9,8 +11,13 @@ namespace TheMovies.Repos
     {
         private List<CinemaMovieShowBooking> entries;
 
+        SqlConnection sqlConnect = new SqlConnection();
+
+        public string connectionString = "Server=10.56.8.35;Database=B_DB07_2020;User Id=B_STUDENT07;Password=B_OPENDB07";
+
         public CinemaMovieShowBookingRepo()
         {
+            sqlConnect.ConnectionString = connectionString;
             entries = new List<CinemaMovieShowBooking>();
             LoadRepo();
         }
@@ -24,37 +31,253 @@ namespace TheMovies.Repos
 
         public CinemaMovieShowBooking GetById(object id)
         {
-            throw new NotImplementedException();
+            foreach (CinemaMovieShowBooking c in entries)
+            {
+                if (c.BookingID == (int)id)
+                    return c;
+            }
+            return null;
         }
 
         public void Add(CinemaMovieShowBooking obj)
         {
-            // Implement this method !
+            if (obj.CinemaName != default)
+            {
+                bool doesItExist = entries.Any(x => x.ToString() == obj.ToString() && x.ToString() == obj.ToString());
+                if (doesItExist)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    entries.Add(obj);
+                    SaveRepo();
+                }
+            }
+            else if (obj.MovieTitle != default)
+            {
+                bool doesItExist = entries.Any(x => x.ToString() == obj.ToString());
+                if (doesItExist)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    entries.Add(obj);
+                    SaveRepo();
+                }
+            }
+            else if (obj.ShowDateTime != default)
+            {
+                bool doesItExist = entries.Any(x => x.ToString() == obj.ToString());
+                if (doesItExist)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    entries.Add(obj);
+                    SaveRepo();
+                }
+            }
+            else
+            {
+                bool doesItExist = entries.Any(x => x.ToString() == obj.ToString());
+                if (doesItExist)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    entries.Add(obj);
+                    SaveRepo();
+                }
+            }
 
-            throw new NotImplementedException();
         }
 
         public void Delete(CinemaMovieShowBooking obj)
         {
-            // Implement this method !
+            if (obj.CinemaName != default && obj.CinemaTown != default)
+            {
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    if (entries[i].CinemaName == obj.CinemaName)
+                    {
+                        entries.RemoveAll(c => c.CinemaName == obj.CinemaName && c.CinemaTown == obj.CinemaTown);
+                        SaveRepo();
+                    }
 
-            throw new NotImplementedException();
+                }
+            }
+            else if (obj.MovieTitle != default)
+            {
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    if (entries[i].MovieTitle == obj.MovieTitle)
+                    {
+                        entries.RemoveAll(c => c.MovieTitle == obj.MovieTitle);
+                        SaveRepo();
+                    }
+                }
+            }
+            else if (obj.ShowDateTime != default)
+            {
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    if (entries[i].ShowDateTime == obj.ShowDateTime)
+                    {
+                        entries.RemoveAll(c => c.ShowDateTime == obj.ShowDateTime);
+                        SaveRepo();
+
+                    }
+                }
+            }
+            else
+                for (int i = 0; i < entries.Count; i++)
+                {
+                    if (entries[i].BookingMail == obj.BookingMail)
+                    {
+                        entries.RemoveAll(c => c.BookingMail == obj.BookingMail);
+                        SaveRepo();
+                    }
+                }
         }
 
         public void Update(CinemaMovieShowBooking obj, CinemaMovieShowBooking newValues)
         {
-            // Implement this method !
 
-            throw new NotImplementedException();
+            if (obj.CinemaName != default)
+            {
+                foreach (CinemaMovieShowBooking c in entries)
+                {
+                    if (c.CinemaName == obj.CinemaName && c.CinemaTown == obj.CinemaTown)
+                    {
+                        c.CinemaName = newValues.CinemaName;
+                        c.CinemaTown = newValues.CinemaTown;
+                    }
+                }
+            }
+            else if (obj.MovieTitle != default)
+            {
+                foreach (CinemaMovieShowBooking c in entries)
+                {
+                    if (c.MovieTitle == obj.MovieTitle)
+                    {
+                        c.MovieTitle = newValues.MovieTitle;
+                        c.MovieGenre = newValues.MovieGenre;
+                        c.MovieDuration = newValues.MovieDuration;
+                        c.MovieDirector = newValues.MovieDirector;
+                        c.MovieReleaseDate = newValues.MovieReleaseDate;
+                    }
+                }
+            }
+            else if (obj.ShowDateTime != default)
+            {
+                foreach (CinemaMovieShowBooking c in entries)
+                {
+                    if (c.ShowDateTime == obj.ShowDateTime)
+                        c.ShowDateTime = newValues.ShowDateTime;
+                }
+            }
+            else
+                foreach (CinemaMovieShowBooking c in entries)
+                {
+                    if (c.BookingMail == obj.BookingMail)
+                    {
+                        c.BookingMail = newValues.BookingMail;
+                        c.BookingPhone = newValues.BookingPhone;
+                    }
+                }
+            SaveRepo();
         }
 
         private void LoadRepo()
         {
-            // Implement this method !
+            sqlConnect.Open();
+            SqlCommand command = new SqlCommand("Select * FROM CinemaMovieShowBooking", sqlConnect);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var entry = new CinemaMovieShowBooking();
+                entry.BookingID = reader.GetInt32(0);
+                entry.CinemaName = reader.IsDBNull(1) != true ?  reader.GetString(1) : default;
+                entry.CinemaTown = reader.IsDBNull(2) != true ? reader.GetString(2) : default;
+                entry.MovieTitle = reader.IsDBNull(3) != true ? reader.GetString(3) : default; 
+                entry.MovieGenre = reader.IsDBNull(4) != true ? reader.GetString(4) : default;
+                entry.MovieDuration = reader.IsDBNull(5) != true ? reader.GetInt32(5) : default;
+                entry.MovieDirector = reader.IsDBNull(6) != true ? reader.GetString(6) : default;
+                entry.MovieReleaseDate = reader.IsDBNull(7) != true ? reader.GetDateTime(7) : default;
+                entry.ShowDateTime = reader.IsDBNull(8) != true ? reader.GetDateTime(8) : default;
+                entry.BookingMail = reader.IsDBNull(9) != true ? reader.GetString(9) : default;
+                entry.BookingPhone = reader.IsDBNull(10) != true ? reader.GetString(10) : default;
+                entries.Add(entry);
+            }
         }
         private void SaveRepo()
         {
-            // Implement this method !
+            string commandTextInsert = "INSERT INTO CinemaMovieShowBooking(CinemaName,CinemaTown,ShowDateTime,MovieTitle,MovieGenre,MovieDuration,MovieDirector,MovieReleaseDate,BookingMail,BookingPhone)" +
+                "VALUES (@CinemaName,@CinemaTown,@ShowDateTime,@MovieTitle,@MovieGenre,@MovieDuration,@MovieDirector,@MovieReleaseDate,@BookingMail,@BookingPhone)";
+            string commandTextUpdate = "UPDATE CinemaMovieShowBooking SET CinemaName = @CinemaName, CinemaTown = @CinemaTown, ShowDateTime = @ShowDateTime, MovieTitle = @MovieTitle, MovieGenre = @MovieGenre " +
+                "WHERE BookingID = @BookingID;  ";
+            using (SqlConnection connect = new SqlConnection(connectionString))
+            {
+                connect.Open();
+                foreach (var c in entries)
+                {
+                    if (c.BookingID == 0)
+                    {
+                        SqlCommand command = new SqlCommand(commandTextInsert, connect);
+
+                        command.Parameters.AddWithValue("@CinemaName", c.CinemaName != default ? (object)c.CinemaName : DBNull.Value);
+                        command.Parameters.AddWithValue("@CinemaTown", c.CinemaTown != default ? (object)c.CinemaTown : DBNull.Value);
+                        command.Parameters.AddWithValue("@ShowDateTime", c.ShowDateTime.ToString(@"yy-M-d hh:mm") != default ? (object)c.ShowDateTime.ToString(@"yy-M-d hh:mm") : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieTitle", c.MovieTitle != default ? (object)c.MovieTitle : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieGenre", c.MovieGenre != default ? (object)c.MovieGenre : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieDuration", c.MovieDuration != default ? (object)c.MovieDuration : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieDirector", c.MovieDirector != default ? (object)c.MovieDirector : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieReleaseDate", c.MovieReleaseDate.ToString(@"yy-M-d") != default ? (object)c.MovieReleaseDate.ToString(@"yy-M-d") : DBNull.Value);
+                        command.Parameters.AddWithValue("@BookingMail", c.BookingMail != default ? (object)c.BookingMail : DBNull.Value);
+                        command.Parameters.AddWithValue("@BookingPhone", c.BookingPhone != default ? (object)c.BookingPhone : DBNull.Value);
+                        try
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+                    else
+                    {
+                        SqlCommand command = new SqlCommand(commandTextUpdate, connect);
+
+                        command.Parameters.AddWithValue("@BookingID", c.BookingID);
+                        command.Parameters.AddWithValue("@CinemaName", c.CinemaName != default ? (object)c.CinemaName : DBNull.Value);
+                        command.Parameters.AddWithValue("@CinemaTown", c.CinemaTown != default ? (object)c.CinemaTown : DBNull.Value);
+                        command.Parameters.AddWithValue("@ShowDateTime", c.ShowDateTime.ToString(@"yy-M-d hh:mm") != default ? (object)c.ShowDateTime.ToString(@"yy-M-d hh:mm") : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieTitle", c.MovieTitle != default ? (object)c.MovieTitle : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieGenre", c.MovieGenre != default ? (object)c.MovieGenre : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieDuration", c.MovieDuration != default ? (object)c.MovieDuration : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieDirector", c.MovieDirector != default ? (object)c.MovieDirector : DBNull.Value);
+                        command.Parameters.AddWithValue("@MovieReleaseDate", c.MovieReleaseDate.ToString(@"yy-M-d") != default ? (object)c.MovieReleaseDate.ToString(@"yy-M-d") : DBNull.Value);
+                        command.Parameters.AddWithValue("@BookingMail", c.BookingMail != default ? (object)c.BookingMail : DBNull.Value);
+                        command.Parameters.AddWithValue("@BookingPhone", c.BookingPhone != default ? (object)c.BookingPhone : DBNull.Value);
+                        try
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+
+
+                }
+            }
         }
     }
 }
